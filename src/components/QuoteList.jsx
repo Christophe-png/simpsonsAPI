a71ec12d8a5f9d1c1f298acd0a86314207513e92
CASTAN <christophecastan4@gmail.com>
2020-10-31 15:28:03 +0100

    install axios

--- src/components/QuoteList.jsx
+++ src/components/QuoteList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./QuoteList.css";
 
-function QuoteListe({ quote }) {
+function QuoteList({ quote }) {
   return (
     <div className="QuoteList">
       <img src={quote.image} alt={quote.character} />
